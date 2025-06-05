7643c6879ebd4cf7e36a336d14010e928ac79662
wltjs6668 <wltjs6668@gmail.com>
2025-06-05 18:11:30 +0900

    [docs]  js 연동 및 swiper 연동 진행중

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// 단축키 ctrl+space = "DOMContentLoaded"
+window.addEventListener("DOMContentLoaded", function () {
+  // main
+  const main = document.querySelector(".main");
+  // 윈도우 스크롤 체크
+  window.addEventListener("scroll", function () {
+    const scY = this.window.scrollY;
+    if (scY > 0) {
+      main.classList.add("main_scroll");
+    } else {
+      main.classList.remove("main_scroll");
+    }
+  });
+});

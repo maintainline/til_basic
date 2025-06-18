db56004d8ecdbf6daa31280efcb52c722714ae48
maintainline <z.seon.dev@gmail.com>
2025-06-18 18:10:14 +0900

    [docs] footer 작업중!

---
+++ js/header.ts
@@ -0,0 +1,69 @@
+console.log("header.js 코드 실행");
+
+// html 의 태그 구조를 모두 읽어 들였다면 그때 찾아라.
+// 아래 구문 외워야 하나요? 외우면 좋음^^
+// const 명사 명사(대문자 시작) ex. <const linkSite>
+
+window.addEventListener("DOMContentLoaded", function () {
+  // 상단 영역
+  const headerTop: Element | null = document.querySelector(".header_top");
+
+  //로고
+  const logo: Element | null = document.querySelector(".logo");
+  //제품 검색
+  const search: Element | null = document.querySelector(".search");
+  //세일 제품
+  const sale: Element | null = document.querySelector(".sale");
+
+  //링크사이트목록
+  const linkSite: Element | null = document.querySelector(".link_site");
+  // 메인메뉴 a 태그들 :  여러개 (all)
+  const mainMenuA: NodeListOf<Element> =
+    document.querySelectorAll(".main_menu li a");
+  console.log(mainMenuA);
+  // 멤버메뉴 a 태그들 : 여러개 (all)
+  const memberMenuA: NodeListOf<Element> =
+    document.querySelectorAll(".member_menu li a");
+  //모바일 메뉴 참조
+  const mobileHeader: Element | null = document.querySelector(".mobile_header");
+  // 웹 브라우저의 스크롤을 체크
+  window.addEventListener("scroll", function () {
+    // 스크롤바의 최상단
+    const scY = window.scrollY;
+    if (scY > 0) {
+      // console.log("스크롤 되었다.");
+      headerTop?.classList.add("header_top_scroll");
+      logo?.classList.add("logo_hide");
+      sale?.classList.add("sale_hide");
+      search?.classList.add("search_scroll");
+      linkSite?.classList.add("link_site_hide");
+      //메인메뉴 class 추가
+      mainMenuA.forEach(function (item) {
+        item.classList.add("height_62");
+        // console.log(item);
+      });
+      //회원메뉴 class 추가
+      memberMenuA.forEach(function (item) {
+        item.classList.add("height_62");
+      });
+
+      mobileHeader?.classList.add("mobile_scroll_line");
+    } else {
+      // console.log("스크롤 최상단.");
+      headerTop?.classList.remove("header_top_scroll");
+      logo?.classList.remove("logo_hide");
+      sale?.classList.remove("sale_hide");
+      search?.classList.remove("search_scroll");
+      linkSite?.classList.remove("link_site_hide");
+
+      mainMenuA.forEach(function (item) {
+        item.classList.remove("height_62");
+      });
+      memberMenuA.forEach(function (item) {
+        item.classList.remove("height_62");
+      });
+
+      mobileHeader?.classList.remove("mobile_scroll_line");
+    }
+  });
+});
